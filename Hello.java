0d2bd40f3d5b451f17c86a8041777dc589a8ef75
svstafford <31293076+svstafford@users.noreply.github.com>
2025-04-11 17:23:39 +0000

    greet method

--- Hello.java
+++ Hello.java
@@ -1,5 +1,23 @@
+import java.util.*;
+
 public class Hello{
+    
+    public void greet(){
+        System.out.println("Hello World from an object!");
+    }
+    
     public static void main (String[] args){
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("What is your name?");
+        String name = scan.nextLine();
+
+        System.out.println("Hello " + name + ", nice to meet you");
+        
+        Hello hello = new Hello();
+
+        hello.greet();
+
         System.out.println("Mr. Stafford is bad at wordle");
     }
 }
